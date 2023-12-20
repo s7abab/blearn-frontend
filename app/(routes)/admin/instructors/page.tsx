1eb873aee351b6814030edac7149fba113d0baef
Shabab <s7abab2k1@gmail.com>
2023-12-20 22:27:33 +0530

    code cleanup done

--- app/(routes)/admin/instructors/page.tsx
+++ app/(routes)/admin/instructors/page.tsx
@@ -3,11 +3,8 @@ import { IUserDataForTable } from "@/@types/interfaces/user/user.interface";
 import CustomTable from "@/app/components/common/CustomTable";
 import Loader from "@/app/components/common/spinners/Loader";
 import { useGetInstructorsQuery } from "@/redux/features/user/userApi";
-import React from "react";
 
-type Props = {};
-
-const InstructorsList = (props: Props) => {
+const InstructorsList = () => {
   const { data, isLoading } = useGetInstructorsQuery({});
   const instructors: IUserDataForTable[] = data?.instructors;
   if (isLoading) {
