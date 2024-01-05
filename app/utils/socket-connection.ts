1b0a24f43102672efb5db2be062a84a4817924f2
Shabab <s7abab2k1@gmail.com>
2024-01-05 21:25:41 +0530

    update socket connection

--- app/utils/socket-connection.ts
+++ app/utils/socket-connection.ts
@@ -1,4 +1,4 @@
 import socketIO from "socket.io-client";
 
 const ENDPOINT = "wss://www.mintapp.online" || "";
-export const SOCKET = socketIO(ENDPOINT, { transports: ["websocket"] });
+export const SOCKET = socketIO(ENDPOINT);
