97e16a2439d1de758b976c3967982a85e5fdcb37
Shabab <s7abab2k1@gmail.com>
2024-01-05 18:21:24 +0530

    updated modal position

--- app/components/common/modals/CustomModal.tsx
+++ app/components/common/modals/CustomModal.tsx
@@ -25,7 +25,7 @@ const CustomModal = ({ isOpen, onClose, children, modalHeader }: Props) => {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
-        className={` w-[380px] outline-none  mx-auto absolute left-[50%] transform translate-x-[-50%] translate-y-[-50%] z-[1001] shadow-xl rounded-lg ${
+        className={` w-[380px] outline-none  mx-auto absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%] z-[1001] shadow-xl rounded-lg ${
           modalHeader
             ? "dark:bg-gradient-to-tr dark:from-gray-950 dark:to-gray-900 bg-gray-500"
             : "bg-white"
