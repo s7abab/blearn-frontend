2912fbfc5276eb18c151a00dae254ee8cd3731e8
Shabab <s7abab2k1@gmail.com>
2024-02-12 21:57:35 +0530

    update footer position add aboutus contactus

--- app/components/common/ui/Footer.tsx
+++ app/components/common/ui/Footer.tsx
@@ -1,32 +1,28 @@
+import Link from "next/link";
+
 const Footer = () => {
   return (
-    <footer className="realative bottom-0 flex w-full flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 border-t border-gray-600 py-6 text-center md:justify-between">
+    <footer className="flex w-full flex-row flex-wrap items-center justify-center mt-11 border-t border-gray-600 py-6 text-center md:justify-between z-50">
       <p className="text-blue-gray font-normal">&copy; 2024 B Learn</p>
       <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
-        <li>
-          <a
-            href="#"
-            className="text-gray-600 dark:text-gray-50 font-normal hover:text-gray-600"
-          >
-            About Us
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            className="text-gray-600 dark:text-gray-50 font-normal hover:text-gray-600"
-          >
-            Contribute
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            className="text-gray-600 dark:text-gray-50 font-normal hover:text-gray-600"
-          >
-            Contact Us
-          </a>
-        </li>
+        <Link
+          href={"/aboutus"}
+          className="text-gray-600 dark:text-gray-50 font-normal hover:text-gray-600"
+        >
+          About Us
+        </Link>
+        <Link
+          href={"https://github.com/s7abab/blearn-backend"}
+          className="text-gray-600 dark:text-gray-50 font-normal hover:text-gray-600"
+        >
+          Contribute
+        </Link>
+        <Link
+          href={"contactus"}
+          className="text-gray-600 dark:text-gray-50 font-normal hover:text-gray-600"
+        >
+          Contact Us
+        </Link>
       </ul>
     </footer>
   );
