0349d1d027ff9862471cbf098ca82eb83d57ea49
Shabab <s7abab2k1@gmail.com>
2024-02-19 21:57:55 +0530

    update video player , prevent screen capturing and downloading

--- app/components/video/VideoPlayer.tsx
+++ app/components/video/VideoPlayer.tsx
@@ -1,6 +1,5 @@
-"use client";
 import useGetS3Link from "@/app/hooks/useGetS3Link";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ReactPlayer from "react-player";
 
 interface Props {
@@ -8,19 +7,38 @@ interface Props {
   height: string;
   handleProgress?: (state: { played: number }) => void;
 }
-// disable right button
+
+// Function to disable right-click context menu
 const disableContextMenu = (event: React.MouseEvent<HTMLDivElement>) => {
   event.preventDefault();
 };
 
 const VideoPlayer = ({ url, height, handleProgress }: Props) => {
   const { s3Url, getFileUrl } = useGetS3Link();
+  const [showOverlay, setShowOverlay] = useState(false);
+
   useEffect(() => {
     if (url) {
       getFileUrl(url);
     }
-    // eslint-disable-next-line
   }, [url]);
+
+  // Check for screen recording software
+  useEffect(() => {
+    const checkScreenRecording = () => {
+      const mediaDevices = navigator.mediaDevices as any;
+      if (mediaDevices.getDisplayMedia || mediaDevices.getDisplayMedia) {
+        setShowOverlay(true); // Show overlay if screen recording is detected
+      }
+    };
+
+    checkScreenRecording();
+
+    const interval = setInterval(checkScreenRecording, 1000); // Check every second
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="relative md:p-2 " onContextMenu={disableContextMenu}>
       <ReactPlayer
@@ -38,6 +56,21 @@ const VideoPlayer = ({ url, height, handleProgress }: Props) => {
           },
         }}
       />
+    
+    {/* disble screen shot and screen record */}
+      {showOverlay && (
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            background: "rgba(0, 0, 0, 0.01)", // Transparent background
+            pointerEvents: "none", // Allow clicks to pass through
+          }}
+        ></div>
+      )}
     </div>
   );
 };
